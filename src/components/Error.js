cb917b8e532642b2556ad730b5e28e00866ec641
apg0000 <129718337+apg0000@users.noreply.github.com>
2025-03-03 14:06:04 +0530

    Update Error.js

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,1 +1,17 @@
-
+import { useRouteError } from "react-router";
+const Error = () => {
+    const err = useRouteError();
+    console.log(err);
+    
+    return (
+        <div>
+            <h1>Opps!!
+                <br/>
+                <q style={{color:"red"}} >Stay Aleart !!!!!!!!!!!!!!!!!!</q >
+            </h1>
+            <h2 style={{color:"dodgerblue"}}>Something went wrong!</h2>
+            
+        </div>
+    )
+}
+export default Error;
